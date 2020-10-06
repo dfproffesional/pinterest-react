131e14e07dac8c636c7a31fe132dfcf56d9401ab
dfproffesional <dfproffesional@gmail.com>
2020-10-06 18:33:26 -0400

    Update login animation2

--- src/layouts/login/Login__Layout.js
+++ src/layouts/login/Login__Layout.js
@@ -1,57 +1,50 @@
-import React, { Component } from 'react';
-import './Login__LayoutStyle.css';
+import React, { Component, useEffect } from "react";
+import "./Login__LayoutStyle.css";
 
+const disableScroll = () => {
+  window.scrollTo(0, 0);
+};
 
+export function Login__Layout(props) {
+  useEffect(() => {
+    window.addEventListener("scroll", disableScroll);
+  });
+  return (
+    <>
+      {/*Header*/}
+      <header className="login-header">
+        <div className="header__left">{props.children[0]}</div>
+        <div className="header__right">{props.children[1]}</div>
+      </header>
 
-export class Login__Layout extends Component {
-    constructor(props){
-        super();
-    }
+      {/*Content*/}
+      <main className="login-content">
+        {/*Content*/}
+        <div className="content__title">{props.children[2]}</div>
 
-    render() {
-       
-        return (
-            <>
-                {/*Header*/}
-                <header className="login-header">
-                    <div className="header__left">
-                        {this.props.children[0]}
-                    </div>
-                    <div className="header__right">
-                        {this.props.children[1]}
-                    </div>
-                </header> 
-                
-                {/*Content*/}
-                <main className="login-content">
-                    {/*Content*/}
-                    <div className="content__title">
-                    {this.props.children[2]}
-                    </div>
+        {/*Colash*/}
+        <div className="login-colash">{props.children[3]}</div>
+      </main>
 
-                    {/*Colash*/}
-                    <div className="login-colash">
-                        {this.props.children[3]}  
-                    </div>
-                </main>
-                
-                {/*Footer*/}
-                <footer className="login-footer">
-                    <nav className="footer__nav">
-                        <a href="#">Terms of Service</a> 
-                        <a href="#">Privacy Policy</a> 
-                        <a href="#">Help</a>
-                        <a href="#">iPhone App</a> 
-                        <a href="#">Android App</a> 
-                        <a href="#">Users</a> 
-                        <a href="#">Collections</a> 
-                        <a href="#">Explore</a>
-                    </nav>
-                </footer>
-                
-            </>
-        )
-    }
+      <div className="login-layer">
+        <button className="layer__down effect">Down</button>
+      </div>
+
+      {/*Footer*/}
+      <footer className="login-footer">
+        <nav className="footer__nav">
+          <a href="#">Terms of Service</a>
+          <a href="#">Privacy Policy</a>
+          <a href="#">Help</a>
+          <a href="#">iPhone App</a>
+          <a href="#">Android App</a>
+          <a href="#">Users</a>
+          <a href="#">Collections</a>
+          <a href="#">Explore</a>
+        </nav>
+      </footer>
+    </>
+  );
 }
 
-export default Login__Layout;
+//export default Login__Layout;
--- src/views/login/Login__View.js
+++ src/views/login/Login__View.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import {Login__Layout as Layout} from '../../layouts/login/Login__Layout';
-import {Header__Menu as Menu } from '../../components/Header__Menu';
-import {Header__Logo as Logo } from '../../components/Header__Logo';
+import React from "react";
+import { Login__Layout as Layout } from "../../layouts/login/Login__Layout";
+import { Header__Menu as Menu } from "../../components/Header__Menu";
+import { Header__Logo as Logo } from "../../components/Header__Logo";
 
-import {Colash__Item as Colash } from '../../components/login/Colash__Item';
+import { Colash__Item as Colash } from "../../components/login/Colash__Item";
 
-import {Content__Title as Title } from '../../components/login/Content__Title';
+import { Content__Title as Title } from "../../components/login/Content__Title";
 
 export default function Login__View() {
-    return (
-        <>
-            <Layout>
-                <Logo />
-                <Menu />
-                <Title />
-                <Colash />
-            </Layout>
-        </>
-    )
+  return (
+    <>
+      <Layout>
+        <Logo />
+        <Menu />
+        <Title />
+        <Colash />
+      </Layout>
+    </>
+  );
 }
